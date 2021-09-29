237ae0f20c8dc1c7e55f82f2db59777b62116c5c
Alekos <alekosmp86@gmail.com>
2021-09-29 18:13:45 -0400

    Added skybox to scene. Also make controls to focus on planet

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import "./App.css";
-import { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Scene from "./components/Scene";
-import Planet from "./models/basic/Planet";
+import Planet from "./components/Planet";
+import Skybox from "./components/Skybox";
 
 function App() {
+  const planet = React.createRef(null);
+  const [controlsTarget, setControlsTarget] = useState([0, 0, 0]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setControlsTarget(planet.current.position);
+    }, 500);
+  }, [planet]);
+
   return (
     <div id="canvas-container" className="vh-100">
-      <Canvas>
-        <Suspense fallback={null}>
+      <Suspense fallback={<span>loading...</span>}>
+        <Canvas camera={{ position: [0, 5, 15], near: 0.1 }}>
+          <Skybox />
           <Scene>
             <Planet
-              position={[0, 0, -10]}
+              ref={planet}
+              position={[0, 0, 0]}
               textures={{
                 map: "resources/textures/Earth/colorMap.jpg",
                 normalMap: "resources/textures/Earth/normalMap.png",
               }}
             />
           </Scene>
-          <OrbitControls />
-        </Suspense>
-      </Canvas>
+          <OrbitControls target={controlsTarget} />
+        </Canvas>
+      </Suspense>
     </div>
   );
 }
