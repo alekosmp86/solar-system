e7d5f6dcc14c8ae0fa7b49b3fe8c87969e592118
Alekos <alekosmp86@gmail.com>
2021-09-30 01:18:54 -0400

    Added a couple of pointlights to scene

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -2,7 +2,8 @@ const Scene = ({ children }) => {
   return (
     <>
       <ambientLight intensity={0.2} />
-      <directionalLight />
+      <pointLight position={[0, 150, 0]} />
+      <pointLight position={[0, -150, 0]} />
       {children}
     </>
   );
